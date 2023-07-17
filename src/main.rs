212acd78013b9ca15592cd59573c8e1eceddcb7f
Martin Rouault <mrouault@hotmail.fr>
2023-07-17 12:32:40 +0200

    Routage

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,47 @@
-use std::str::FromStr;
-
 use leptos::*;
+use leptos_router::*;
+use leptos_meta::*;
 
 fn main() {
-    let projs = vec![Projet::Jeu(JeuData { titre: String::from_str("yoo").unwrap() }), Projet::Programme(ProgrammeData { nom: String::from_str("Pouineatr").unwrap() })]; 
-
-    mount_to_body(|cx| view! {cx , <App projets=projs/> })
-
-}
-
-enum Projet {
-    Jeu(JeuData),
-    Programme(ProgrammeData),
-}
-
-struct JeuData {
-    titre:String
-}
-
-struct ProgrammeData {
-    nom:String,
-}
-
-#[derive(Clone, PartialEq)]
-enum Page {
-    Page1,
-    Page2,
+    mount_to_body(|cx| view! {cx , <App/> })
 }
 
 #[component]
-fn App (cx: Scope, projets:Vec<Projet>) -> impl IntoView {
-    
+fn App (cx: Scope) -> impl IntoView {
 
-    let (page, set_page) = create_signal(cx, Page::Page1);
-    
     view! { 
         cx,
         <Style>{include_str!("style.css")}</Style>
-        <button
-            on:click=move |_| {
-                set_page(
-                    match page() {
-                        Page::Page1 => Page::Page2,
-                        Page::Page2 => Page::Page1,
-                    }    
-                )
-            }
-            >
-                "Switch page"
-        </button>
-        
-        <div class:project_list=true>
-            {
-                projets.iter().map(|p| view! {
-                    cx,
-                    <p>{
-                        match p {
-                            Projet::Jeu(n) => n.titre.clone(),
-                            Projet::Programme(n) => n.nom.clone(),
-                        }
-                    }</p>
-                }).collect::<Vec<_>>()
-            }
-        </div>
+        <Router>
+            <main>
+                <Routes>
+                    <Route path="/" view=|cx| view! { cx, <Home/> } />
+                    <Route path="/explosivite" view=|cx| view! { cx, <Explosivite/> } />
+                </Routes>
+            </main>
+            <nav>
+                <A href="/">"Home"</A>
+                <A href="/explosivite">"Explosivite"</A>
+            </nav>
 
-
-        <Show
-            when= move || page.get() == Page::Page1
-            fallback= |cx| view! {cx, "Geauie"}
-            >
-                <Page1/>
-        </Show>
+        </Router>
     }
 }
 
+
 #[component]
-fn Page1(cx: Scope) -> impl IntoView {
+fn Explosivite(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <div class="page">
+            <h1>"Explosivité"</h1>
+            {"Explosivité est un plateformer dans lequel l'énergie pour effectuer des actions spéciales comme des sauts est aussi la vie du personnage."}
+        </div>
+    }
+}
+
+
+#[component]
+fn Home(cx: Scope) -> impl IntoView {
     view! { cx,
         <div>
             <h1>"Salut page 1"</h1>
