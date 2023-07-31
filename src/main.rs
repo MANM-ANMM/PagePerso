bb5cc13e22a5c8f558a2b9977eead054866374e9
Martin Rouault <mrouault@hotmail.fr>
2023-07-31 19:32:24 +0200

    reformat

--- src/main.rs
+++ src/main.rs
@@ -4,86 +4,88 @@ use leptos_router::*;
 use const_format::concatcp;
 
 fn main() {
-    mount_to_body(|cx| view! {cx , <App/> })
+	mount_to_body(|cx| view! {cx , <App/> })
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    view! {
-        cx,
-        <Style>{include_str!("style.css")}</Style>
-        <Router>
-            <main>
-            <Routes>
-                <Route path={PATH_HOME} view=|cx| view! { cx, <Home/> } />
-                <Route path={PATH_EXPLOSIVITE} view=|cx| view! { cx, <Explosivite/> } />
-                <Route path={PATH_TEST} view=|cx| view! { cx, <Test/> } />
-                <Route path="/*any" view=|cx| view! { cx, <Home/> } />
-            </Routes>
-            <div class="foot_space"/>
-            </main>
-            <Navigation/>
-        </Router>
-    }
+	view! {
+		cx,
+		<Style>{include_str!("style.css")}</Style>
+		<Router>
+			<main>
+			<Routes>
+				<Route path={PATH_HOME} view=|cx| view! { cx, <Home/> } />
+				<Route path={PATH_EXPLOSIVITE} view=|cx| view! { cx, <Explosivite/> } />
+				<Route path={PATH_TEST} view=|cx| view! { cx, <Test/> } />
+				<Route path="/*any" view=|cx| view! { cx, <Home/> } />
+			</Routes>
+			<div class="foot_space"/>
+			</main>
+			<Navigation/>
+		</Router>
+	}
 }
 
 #[component]
 fn Test(cx: Scope) -> impl IntoView {
-    view! { cx,
-    <div class="test">
-        "test"
-    </div>
-    }
+	view! { cx,
+	<div class="test">
+		"test"
+	</div>
+	}
 }
 
 #[component]
 fn Explosivite(cx: Scope) -> impl IntoView {
-    view! { cx,
-    <div class="explosivite">
-        <h1>"Explosivité"</h1>
-        <p>"Explosivité est un plateformer dans lequel l'énergie pour effectuer des actions spéciales comme des sauts est aussi la vie du personnage."</p>
-        <img src={PATH_ROOT.to_owned() + "img/explosivite.png"} alt="Image titre du jeu Explosivité"/>
+	view! { cx,
+	<div class="explosivite">
+		<h1>"Explosivité"</h1>
+		<p>"Explosivité est un plateformer dans lequel l'énergie pour effectuer des actions spéciales comme des sauts est aussi la vie du personnage."</p>
+		<img src={PATH_ROOT.to_owned() + "img/explosivite.png"} alt="Image titre du jeu Explosivité"/>
 
-        <iframe src="https://itch.io/embed/893284" width=552 height=167 frameborder=0>
-            <a href="https://anmm.itch.io/explosivite">
-                "Explosivite by ANMM"
-            </a>
-        </iframe>
+		<iframe src="https://itch.io/embed/893284" width=552 height=167 frameborder=0>
+			<a href="https://anmm.itch.io/explosivite">
+				"Explosivite by ANMM"
+			</a>
+		</iframe>
 
-        <p>"J'ai créé Explosivité pendant ma première année de DUT (2020-2021) avec Godot."</p>
-    </div>
-    }
+		<p>"J'ai créé Explosivité pendant ma première année de DUT (2020-2021) avec Godot."</p>
+	</div>
+	}
 }
 
 #[component]
 fn Home(cx: Scope) -> impl IntoView {
-    view! { cx,
-        <div class="home">
-            <h1>"Martin Rouault"</h1>
-            <p>"Je suis étudiant en informatique en 2ème année d'école d'ingénieur."</p>
-            <p>"Ce site expose une partie des projets que j'ai créé ces dernières années. Le code de ce site est disponible sur "<a href="https://github.com/MANM-ANMM/PagePerso">github</a>"."</p>
-            <p>"La plus part des projets ici sont des jeux-vidéo développés dans mon temps libre. Soit sous forme de projet au long court, soit en game jam de 48 heures."</p>
-        </div>
-    }
+	view! { cx,
+		<div class="home">
+			<h1>"Martin Rouault"</h1>
+			<p>"Je suis étudiant en informatique en 2ème année d'école d'ingénieur."</p>
+			<p>"Ce site expose une partie des projets que j'ai créé ces dernières années. Le code de ce site est disponible sur "<a href="https://github.com/MANM-ANMM/PagePerso">github</a>"."</p>
+			<p>"La plus part des projets ici sont des jeux-vidéo développés dans mon temps libre. Soit sous forme de projet au long court, soit en game jam de 48 heures."</p>
+		</div>
+	}
 }
 
 #[component]
 fn Navigation(cx: Scope) -> impl IntoView {
-    view! { cx,
-        <nav>
-            <A href={PATH_HOME}>"Home"</A>
-            <A href={PATH_EXPLOSIVITE}>"Explosivite"</A>
-            <A href={PATH_TEST}>"Test"</A>
-        </nav>
-    }
+	view! { cx,
+		<nav>
+			<A href={PATH_HOME}>"Home"</A>
+			<A href={PATH_EXPLOSIVITE}>"Explosivite"</A>
+			<A href={PATH_TEST}>"Test"</A>
+		</nav>
+	}
 }
 
 
 const PATH_ROOT : &str = match cfg!(debug_assertions) {
-    true => "/",
-    false => "/~marouault/",
+	true => "/",
+	false => "/~marouault/",
 };
 
 static PATH_HOME: &str = PATH_ROOT;
 static PATH_EXPLOSIVITE: &str = concatcp!(PATH_ROOT, "explosivite");
 static PATH_TEST: &str = concatcp!(PATH_ROOT, "test");
+
+
